e7a178574663ec1249ae468a29c333b43c3f65ee
vasyl-dev <up.vasyl@gmail.com>
2022-06-15 22:14:26 +0300

    first homework

--- script.js
+++ script.js
@@ -23,3 +23,19 @@
 
 'use strict';
 
+const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false,
+};
+
+const a = prompt('Один из последних просмотренных фильмов?', ''),
+    b = prompt('На сколько оцените его?', '');
+
+personalMovieDB.movies[a] = b;
+
+console.log(personalMovieDB.movies);
